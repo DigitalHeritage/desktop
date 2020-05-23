243b621b0047079f803f583e9dd35b8a9191cdf5
Gautier Michelin <gautier.michelin@gmail.com>
2020-05-23 19:44:32 +0200

    main code

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,65 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img src="../public/icon.png" style="height:70%">
+        </a>
+
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <router-link class="navbar-item" to="/">Home</router-link>
+
+          <router-link class="navbar-item" to="/collections">Collections</router-link> 
+          <router-link class="navbar-item" to="/collection">Collection</router-link> 
+          <router-link class="navbar-item" to="/detail">Detail</router-link> 
+          <router-link class="navbar-item" to="/about">About</router-link>
+
+          <div class="navbar-item has-dropdown is-hoverable" style="display:none;">
+            <a class="navbar-link">
+              More
+            </a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item">
+                About
+              </a>
+              <a class="navbar-item">
+                Jobs
+              </a>
+              <a class="navbar-item">
+                Contact
+              </a>
+              <hr class="navbar-divider">
+              <a class="navbar-item">
+                Report an issue
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a class="button is-light">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
     <router-view />
   </div>
 </template>
@@ -13,8 +69,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+require('./assets/digitalheritage.scss');
+require('../node_modules/material-design-icons/iconfont/material-icons.css');
+import Artworks from '../public/artworks-guardian.json';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  data: {
+    artworks: Artworks
+  },
   render: h => h(App)
 }).$mount("#app");
